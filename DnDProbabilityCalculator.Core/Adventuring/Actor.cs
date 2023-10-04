ffcba9354f8e61bd0d3627035105a9044e0501d8
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-10-04 02:46:12 +0200

    todo

--- DnDProbabilityCalculator.Core/Adventuring/Actor.cs
+++ DnDProbabilityCalculator.Core/Adventuring/Actor.cs
@@ -5,5 +5,5 @@ public class Actor
     public string Name { get; init; } = string.Empty;
     public AbilityScores AbilityScores { get; init; } = new();
 
-    // todo: saving throws
+    // todo: builder to create a character
 }
