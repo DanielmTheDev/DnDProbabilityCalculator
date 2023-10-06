5e91c3bfbbbfd247246c25db12afca729e5a861b
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-10-06 16:24:53 +0200

    Add Configuratoin

--- DnDProbabilityCalculator.Console/Program.cs
+++ DnDProbabilityCalculator.Console/Program.cs
@@ -1,10 +1,26 @@
-﻿// var serviceProvider = new ServiceCollection()
-//     .AddScoped<ICalculator, Calculator>()
-//     .BuildServiceProvider();
-//
-// var calculator = serviceProvider.GetService<ICalculator>();
+﻿using DnDProbabilityCalculator.Infrastructure.Actors;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
+var services = new ServiceCollection();
+AddConfiguration(services);
 
-// var jsonString = File.ReadAllText(@"C:\Users\DanielMuckelbauer\AppData\Roaming\JetBrains\Rider2023.2\scratches\scratch.json");
+using var serviceProvider = services.BuildServiceProvider();
+var mySettings = serviceProvider.GetService<IOptions<FileRepositoryOptions>>()!.Value;
+
+// ReSharper disable once LocalizableElement
+Console.WriteLine($"Setting1: {mySettings.FilePath}");
+
+return;
+
+void AddConfiguration(ServiceCollection serviceCollection)
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    var configuration = builder.Build();
+    serviceCollection.Configure<FileRepositoryOptions>(configuration.GetSection("FileRepository"));
+}
 // var party = Party.FromJsonString(jsonString);
 // party.Dump();
