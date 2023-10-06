c3997df514d4095e61a92944fea51a92956d3032
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-10-06 17:29:43 +0200

    PartyService and test

---
+++ DnDProbabilityCalculator.Application.Tests/PartyServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using DnDProbabilityCalculator.Core.Adventuring;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DnDProbabilityCalculator.Application.Tests;
+
+[TestClass]
+public class PartyServiceTests
+{
+    private PartyService _partyService = null!;
+    private Mock<IPartyRepository> _repositoryMock = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _repositoryMock = new();
+        _partyService = new(_repositoryMock.Object);
+    }
+
+    [TestMethod]
+    public void Get_WhenCalled_ReturnsPartyFromRepository()
+    {
+        // Arrange
+        _repositoryMock.Setup(repository => repository.Get()).Returns(GetValidParty());
+
+        // Act
+        var party = _partyService.Get();
+
+        // Assert
+        Assert.AreEqual(2, party.Characters.Count);
+        Assert.AreEqual(GetValidParty().Characters[0].AbilityScores.Strength, party.Characters[0].AbilityScores.Strength);
+        Assert.AreEqual(GetValidParty().Characters[0].AbilityScores.Dexterity, party.Characters[0].AbilityScores.Dexterity);
+        Assert.AreEqual(GetValidParty().Characters[0].AbilityScores.Constitution, party.Characters[0].AbilityScores.Constitution);
+        Assert.AreEqual(GetValidParty().Characters[0].AbilityScores.Intelligence, party.Characters[0].AbilityScores.Intelligence);
+        Assert.AreEqual(GetValidParty().Characters[0].AbilityScores.Wisdom, party.Characters[0].AbilityScores.Wisdom);
+        Assert.AreEqual(GetValidParty().Characters[0].AbilityScores.Charisma, party.Characters[0].AbilityScores.Charisma);
+
+        Assert.AreEqual(GetValidParty().Characters[1].AbilityScores.Strength, party.Characters[1].AbilityScores.Strength);
+        Assert.AreEqual(GetValidParty().Characters[1].AbilityScores.Dexterity, party.Characters[1].AbilityScores.Dexterity);
+        Assert.AreEqual(GetValidParty().Characters[1].AbilityScores.Constitution, party.Characters[1].AbilityScores.Constitution);
+        Assert.AreEqual(GetValidParty().Characters[1].AbilityScores.Intelligence, party.Characters[1].AbilityScores.Intelligence);
+        Assert.AreEqual(GetValidParty().Characters[1].AbilityScores.Wisdom, party.Characters[1].AbilityScores.Wisdom);
+        Assert.AreEqual(GetValidParty().Characters[1].AbilityScores.Charisma, party.Characters[1].AbilityScores.Charisma);
+    }
+
+    private static Party GetValidParty()
+        => new()
+        {
+            Characters = new List<Actor>
+            {
+                Actor.New()
+                    .WithStrength(1)
+                    .WithDexterity(2, true)
+                    .WithConstitution(3)
+                    .WithWisdom(4)
+                    .WithIntelligence(5)
+                    .WithCharisma(6)
+                    .WithProficiency(3)
+                    .Build(),
+                Actor.New()
+                    .WithStrength(6)
+                    .WithDexterity(5)
+                    .WithConstitution(4, true)
+                    .WithWisdom(3)
+                    .WithIntelligence(2, true)
+                    .WithCharisma(1)
+                    .WithProficiency(2)
+                    .Build()
+            }
+        };
+}
---
+++ DnDProbabilityCalculator.Application/PartyService.cs
@@ -0,0 +1,14 @@
+﻿using DnDProbabilityCalculator.Core.Adventuring;
+
+namespace DnDProbabilityCalculator.Application;
+
+public class PartyService
+{
+    private readonly IPartyRepository _repository;
+
+    public PartyService(IPartyRepository repository)
+        => _repository = repository;
+
+    public Party Get()
+        => _repository.Get();
+}
