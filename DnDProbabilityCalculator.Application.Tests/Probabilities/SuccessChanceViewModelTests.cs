e4b90c39051d4ec9217186553c795fd54735fa0e
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-10-14 10:28:20 +0200

    Color tests

---
+++ DnDProbabilityCalculator.Application.Tests/Probabilities/SuccessChanceViewModelTests.cs
@@ -0,0 +1,58 @@
+﻿using DnDProbabilityCalculator.Application.Probabilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DnDProbabilityCalculator.Application.Tests.Probabilities;
+
+[TestClass]
+public class SuccessChanceViewModelTests
+{
+    [TestMethod]
+    public void Implicitly_Converts_ValueToDouble()
+    {
+        // Arrange
+        var chance = new SuccessChanceViewModel(5.0);
+
+        // Act
+        double chanceAsDouble = chance;
+
+        // Assert
+        Assert.AreEqual(5.0, chanceAsDouble);
+    }
+
+    [TestMethod]
+    public void Implicitly_Converts_DoubleViewModel()
+    {
+        // Arrange
+        const double chanceAsDouble = 0.5d;
+
+        // Act
+        SuccessChanceViewModel chance = chanceAsDouble;
+
+        // Assert
+        Assert.AreEqual(chanceAsDouble, chance.Chance);
+    }
+
+    [TestMethod]
+    [DataRow(0.0d, "red")]
+    [DataRow(0.25d, "red")]
+    [DataRow(0.26d, "orange4_1")]
+    [DataRow(0.45d, "orange4_1")]
+    [DataRow(0.46d, "yellow")]
+    [DataRow(0.65d, "yellow")]
+    [DataRow(0.66d, "turquoise2")]
+    [DataRow(0.85d, "turquoise2")]
+    [DataRow(0.86d, "green")]
+    [DataRow(double.MaxValue, "green")]
+    public void ToString_WhenCalled_WrapsResultInColor(double chanceAsDouble, string expectedColor)
+    {
+        // Arrange
+        var chance = new SuccessChanceViewModel(chanceAsDouble);
+
+        // Act
+        var chanceAsString = chance.ToString();
+
+        // Assert
+        Assert.IsTrue(chanceAsString.Contains(expectedColor));
+    }
+
+}
--- DnDProbabilityCalculator.Application/Probabilities/SuccessChanceViewModel.cs
+++ DnDProbabilityCalculator.Application/Probabilities/SuccessChanceViewModel.cs
@@ -13,10 +13,10 @@ public record SuccessChanceViewModel(double Chance)
         var successChanceAsString = Chance.ToString("P0");
         return Chance switch
         {
-            < 0.25 => $"[red]{successChanceAsString}[/]",
-            < 0.45 => $"[orange4_1]{successChanceAsString}[/]",
-            < 0.65 => $"[yellow]{successChanceAsString}[/]",
-            < 0.85 => $"[turquoise2]{successChanceAsString}[/]",
+            <= 0.25 => $"[red]{successChanceAsString}[/]",
+            <= 0.45 => $"[orange4_1]{successChanceAsString}[/]",
+            <= 0.65 => $"[yellow]{successChanceAsString}[/]",
+            <= 0.85 => $"[turquoise2]{successChanceAsString}[/]",
             _ => $"[green]{successChanceAsString}[/]",
         };
     }
