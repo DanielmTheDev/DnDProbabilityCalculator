27a025e78efdf175c26b6c889ce76759ca254ecd
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-10-28 17:59:22 +0200

    Working but ugly GetHitTable

--- DnDProbabilityCalculator.Application/Probabilities/GetHitTable.cs
+++ DnDProbabilityCalculator.Application/Probabilities/GetHitTable.cs
@@ -5,19 +5,24 @@ namespace DnDProbabilityCalculator.Application.Probabilities;
 public class GetHitTable
 {
     public required List<string> AttackModifiers { get; set; }
-    public required List<IEnumerable<string>> Probabilities{ get; init; }
+    public required List<IEnumerable<string>> Probabilities { get; init; }
 
-    private GetHitTable() { }
+    private GetHitTable()
+    {
+    }
 
     public static GetHitTable FromActor(Actor actor, int[] attackModifiers)
     {
         var attackModifierRow = new List<string> { "#Attacks/Modifier" }.Concat(attackModifiers.Select(modifier => modifier.ToString()));
+        var rows = Enumerable.Range(1, 3)
+            .Select(numberOfAttacks => new List<string> { $"{numberOfAttacks}" }.Concat(attackModifiers
+                .Select(attackModifier => actor.CalculateGetHitProbabilities(attackModifier, numberOfAttacks)
+                    .Probabilities.Aggregate("", (cell, probability) => cell + $"hits: {probability.NumberOfHits}: {probability.Probability:P2}\n")))).ToList();
 
-        var attackRow = attackModifiers.Select(attackModifier => actor.CalculateGetHitProbabilities(attackModifier, 5)).ToList();
         return new()
         {
             AttackModifiers = attackModifierRow.ToList(),
-            Probabilities = null
+            Probabilities = rows
         };
     }
 }
