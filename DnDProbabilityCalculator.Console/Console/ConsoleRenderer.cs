a55521590581e9022cb99cd77f576236e47cb2aa
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-10-31 00:14:03 +0100

    Rename

--- DnDProbabilityCalculator.Console/Console/ConsoleRenderer.cs
+++ DnDProbabilityCalculator.Console/Console/ConsoleRenderer.cs
@@ -5,11 +5,11 @@ namespace DnDProbabilityCalculator.Console.Console;
 
 public class ConsoleRenderer : IConsoleRenderer
 {
-    private readonly ITableContextService _tableContextContextService;
+    private readonly ITableContextService _tableContextService;
 
-    public ConsoleRenderer(ITableContextService tableContextContextService)
+    public ConsoleRenderer(ITableContextService tableContextService)
     {
-        _tableContextContextService = tableContextContextService;
+        _tableContextService = tableContextService;
     }
 
     public void Start()
@@ -18,7 +18,7 @@ public class ConsoleRenderer : IConsoleRenderer
         var attackModifiers = Enumerable.Range(-1, 7).ToArray();
         var numberOfAttacks = 2;
 
-        var tableContext = _tableContextContextService.Get(dcs, attackModifiers, numberOfAttacks);
+        var tableContext = _tableContextService.Get(dcs, attackModifiers, numberOfAttacks);
         var table = new Table();
         tableContext.ForEach(data => table.AddColumn(data.ActorName));
 
@@ -26,7 +26,7 @@ public class ConsoleRenderer : IConsoleRenderer
             .Start(context =>
             {
                 table.RerenderRows(tableContext, context);
-                context.Refresh();
+
                 var quit = false;
                 while (!quit)
                 {
@@ -37,12 +37,12 @@ public class ConsoleRenderer : IConsoleRenderer
                             if (numberOfAttacks == 1)
                                 break;
                             numberOfAttacks--;
-                            var newTableContext = _tableContextContextService.Get(dcs, attackModifiers, numberOfAttacks);
+                            var newTableContext = _tableContextService.Get(dcs, attackModifiers, numberOfAttacks);
                             table.RerenderRows(newTableContext, context);
                             break;
                         case ConsoleKey.UpArrow:
                             numberOfAttacks++;
-                            newTableContext = _tableContextContextService.Get(dcs, attackModifiers, numberOfAttacks);
+                            newTableContext = _tableContextService.Get(dcs, attackModifiers, numberOfAttacks);
                             table.RerenderRows(newTableContext, context);
                             break;
                         case ConsoleKey.Q:
