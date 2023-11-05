6c6ead35962f2da7de66153ed06f2ee483839b63
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-11-05 19:11:28 +0100

    DeliverHittable

---
+++ DnDProbabilityCalculator.Application/Table/DeliverHitTable.cs
@@ -0,0 +1,14 @@
+﻿using DnDProbabilityCalculator.Core.Adventuring;
+
+namespace DnDProbabilityCalculator.Application.Table;
+
+public class DeliverHitTable
+{
+    public static DeliverHitTable FromActor(Actor actor, int[] armorClasses, int numberOfAttacks)
+    {
+        throw new NotImplementedException();
+    }
+
+    public required IEnumerable<string> ArmorClasses { get; set; }
+    public required List<IEnumerable<string>> Probabilities { get; init; }
+}
