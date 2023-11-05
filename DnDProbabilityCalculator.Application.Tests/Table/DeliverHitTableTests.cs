fe8bff3fa197347d633323dd38e5594dacecb22b
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-11-05 19:07:09 +0100

    Test

---
+++ DnDProbabilityCalculator.Application.Tests/Table/DeliverHitTableTests.cs
@@ -0,0 +1,41 @@
+﻿using DnDProbabilityCalculator.Application.Table;
+using DnDProbabilityCalculator.Core.Adventuring;
+using DnDProbabilityCalculator.Core.Adventuring.Abilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DnDProbabilityCalculator.Application.Tests.Table;
+
+[TestClass]
+public class DeliverHitTableTests
+{
+    [TestMethod]
+    public void FromActor_WhenCalled_ReturnsDeliverHitChanceTable()
+    {
+        // Arrange
+        var actor = GetValidActor();
+
+        // Act
+        var tableData = DeliverHitTable.FromActor(actor, new[] { 10, 11, 12 }, 2);
+
+        // Assert
+        new List<string> { "2 ","10", "11", "12" }.AssertElementsAreContainedIn(tableData.ArmorClasses);
+        new List<string> { "0", "6%", "4%", "2%" }.AssertElementsAreContainedIn(tableData.Probabilities[0]);
+        new List<string> { "1", "38%", "32%", "26%" }.AssertElementsAreContainedIn(tableData.Probabilities[1]);
+        new List<string> { "2", "56%", "64%", "72%" }.AssertElementsAreContainedIn(tableData.Probabilities[2]);
+    }
+
+    private static Actor GetValidActor()
+        => Actor.New()
+            .WithName("Durak")
+            .WithStrength(10)
+            .WithDexterity(12, true)
+            .WithConstitution(14)
+            .WithWisdom(15)
+            .WithIntelligence(16)
+            .WithCharisma(8)
+            .WithProficiency(9)
+            .WithArmorClass(5)
+            .WithNumberOfAttacks(2)
+            .WithAttackAbility(AbilityScoreType.Charisma)
+            .Build();
+}
