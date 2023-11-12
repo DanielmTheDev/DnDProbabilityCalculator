34458325c5717cb715367d400b30c740abc70a5e
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-11-12 19:28:41 +0100

    Expand table

--- DnDProbabilityCalculator.Console/Console/LiveTableExtensions.cs
+++ DnDProbabilityCalculator.Console/Console/LiveTableExtensions.cs
@@ -10,24 +10,37 @@ public static class LiveTableExtensions
         theTable.Rows.Clear();
         theTable.AddRow(CreateSavingThrowTables(tableContext));
         theTable.AddRow(CreateReceiveHitTables(tableContext));
+        theTable.AddRow(CreateDeliverHitTables(tableContext));
         context.Refresh();
     }
-
-    private static IEnumerable<Table> CreateReceiveHitTables(IEnumerable<TableContext> allActorTables)
-        => allActorTables.Select(actorTable =>
-        {
-            var table = new Table();
-            table.AddColumns(actorTable.ReceiveHitTable.AttackModifiers.ToArray());
-            actorTable.ReceiveHitTable.Probabilities.ForEach(row => table.AddRow(row.ToArray()));
-            return table;
-        }).ToList();
 
     private static IEnumerable<Table> CreateSavingThrowTables(IEnumerable<TableContext> allActorTables)
         => allActorTables.Select(actorTable =>
         {
             var table = new Table();
+            table.Expand();
             table.AddColumns(actorTable.SavingThrowTable.Dcs.ToArray());
             actorTable.SavingThrowTable.Probabilities.ForEach(row => table.AddRow(row.ToArray()));
+            return table;
+        }).ToList();
+
+    private static IEnumerable<Table> CreateReceiveHitTables(IEnumerable<TableContext> allActorTables)
+        => allActorTables.Select(actorTable =>
+        {
+            var table = new Table();
+            table.Expand();
+            table.AddColumns(actorTable.ReceiveHitTable.AttackModifiers.ToArray());
+            actorTable.ReceiveHitTable.Probabilities.ForEach(row => table.AddRow(row.ToArray()));
+            return table;
+        }).ToList();
+
+    private static IEnumerable<Table> CreateDeliverHitTables(IEnumerable<TableContext> allActorTables)
+        => allActorTables.Select(actorTable =>
+        {
+            var table = new Table();
+            table.Expand();
+            table.AddColumns(actorTable.DeliverHitTable.ArmorClasses.ToArray());
+            actorTable.DeliverHitTable.Probabilities.ForEach(row => table.AddRow(row.ToArray()));
             return table;
         }).ToList();
 }
