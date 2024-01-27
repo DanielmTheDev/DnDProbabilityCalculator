1e0f6863fefb8ec872b90334f07c55d2cf4312f9
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-01-27 22:20:04 +0100

    Function

---
+++ DndProbabilityCalculatorFunctions/SaveParty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DndProbabilityCalculatorFunctions;
+
+public static class SaveParty
+{
+    [FunctionName("SaveParty")]
+    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+    {
+        try
+        {
+            return new OkObjectResult("Test");
+        }
+        catch (Exception e)
+        {
+            return new OkObjectResult(e.Message);
+        }
+    }
+}
