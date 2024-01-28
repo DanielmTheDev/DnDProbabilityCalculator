0ca25790b34f04446c00bccfbf19600e22972804
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-01-28 20:55:16 +0100

    Remove warninings + authentication component

--- DndProbabilityCalculatorFunctions/SaveParty.cs
+++ DndProbabilityCalculatorFunctions/SaveParty.cs
@@ -1,28 +1,25 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace DndProbabilityCalculatorFunctions;
 
 public static class SaveParty
 {
     [FunctionName("SaveParty")]
-    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+    public static Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
     {
         try
         {
-            return new OkObjectResult("Test");
+            return Task.FromResult<IActionResult>(new OkObjectResult("Test"));
         }
         catch (Exception e)
         {
-            return new OkObjectResult(e.Message);
+            return Task.FromResult<IActionResult>(new OkObjectResult(e.Message));
         }
     }
 }
