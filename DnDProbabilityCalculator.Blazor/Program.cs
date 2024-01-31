c324f0cc0564821906fe42fc9b375748bcac6a91
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-01-31 11:01:05 +0100

    Syntactic sugar

--- DnDProbabilityCalculator.Blazor/Program.cs
+++ DnDProbabilityCalculator.Blazor/Program.cs
@@ -22,15 +22,10 @@ builder.Services.AddTransient<IPartyRepository, PartyInlineRepository>();
 builder.Services.AddTransient<ITableContextFactory, TableContextFactory>();
 builder.Services.AddScoped<IFileAccessor, FileAccessor>();
 
-builder.Services.AddScoped<AuthorizationMessageHandler>(provider =>
-{
-    var handler = new AuthorizationMessageHandler(provider.GetRequiredService<IAccessTokenProvider>(), provider.GetRequiredService<NavigationManager>());
-    handler.ConfigureHandler(
-        authorizedUrls: ["https://dnd-probability-calculator-functions.azurewebsites.net"],
-        scopes: ["https://dadamucki.onmicrosoft.com/a2cfc276-854c-44fc-b7aa-5706508ed32c/API.Access"]
-    );
-    return handler;
-});
+builder.Services.AddScoped<AuthorizationMessageHandler>(provider
+    => new AuthorizationMessageHandler(provider.GetRequiredService<IAccessTokenProvider>(), provider.GetRequiredService<NavigationManager>())
+        .ConfigureHandler(authorizedUrls: ["https://dnd-probability-calculator-functions.azurewebsites.net"],
+            scopes: ["https://dadamucki.onmicrosoft.com/a2cfc276-854c-44fc-b7aa-5706508ed32c/API.Access"]));
 
 builder.Services
     .AddHttpClient("B2CSandbox.ServerAPI", client => client.BaseAddress = new("https://dnd-probability-calculator-functions.azurewebsites.net"))
