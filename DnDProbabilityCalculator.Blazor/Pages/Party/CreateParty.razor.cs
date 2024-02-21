97e446d324ab0ee2d9f3c31ad81019739a94d02c
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-02-21 17:31:45 +0100

    General Form Layout

--- DnDProbabilityCalculator.Blazor/Pages/Party/CreateParty.razor.cs
+++ DnDProbabilityCalculator.Blazor/Pages/Party/CreateParty.razor.cs
@@ -6,6 +6,14 @@ public partial class CreateParty
 {
     private readonly CreatePartyDto _party = new();
 
+    protected override void OnInitialized()
+    {
+        if (!_party.Characters.Any())
+        {
+            _party.Characters.Add(new());
+        }
+    }
+
     private static void SubmitValidForm()
     {
         Console.Out.Write("Valid Submit");
