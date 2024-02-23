da6ac28f3c30b40ac439e8a8f394e4eb9f07b548
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-02-23 18:00:58 +0100

    Extract card

---
+++ DnDProbabilityCalculator.Blazor/PartyCreation/CreateCharacterCard.razor.cs
@@ -0,0 +1,19 @@
+﻿using DnDProbabilityCalculator.Core.Adventuring.Abilities;
+using DnDProbabilityCalculator.Shared.PartyCreation;
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace DnDProbabilityCalculator.Blazor.PartyCreation;
+
+public partial class CreateCharacterCard
+{
+    [Parameter]
+    public CreateCharacterDto Character { get; set; } = null!;
+
+    private IEnumerable<Option<AbilityScoreType>> GetAbilityTypeOptions()
+        => Enum.GetValues<AbilityScoreType>().Select(value => new Option<AbilityScoreType>
+        {
+            Value = value,
+            Text = value
+        });
+}
--- DnDProbabilityCalculator.Blazor/PartyCreation/CreatePartyPage.razor.cs
+++ DnDProbabilityCalculator.Blazor/PartyCreation/CreatePartyPage.razor.cs
@@ -1,7 +1,5 @@
-﻿using DnDProbabilityCalculator.Core.Adventuring.Abilities;
-using DnDProbabilityCalculator.Shared.PartyCreation;
+﻿using DnDProbabilityCalculator.Shared.PartyCreation;
 using Microsoft.AspNetCore.Components;
-using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace DnDProbabilityCalculator.Blazor.PartyCreation;
 
@@ -20,13 +18,6 @@ public partial class CreatePartyPage
             _party.Characters.Add(new());
         }
     }
-    private IEnumerable<Option<AbilityScoreType>> GetAbilityTypeOptions()
-        => Enum.GetValues<AbilityScoreType>().Select(value => new Option<AbilityScoreType>
-        {
-            Value = value,
-            Text = value
-        });
-
     private async Task Submit()
         => _result = await PartySaver.Save(_party);
 }
